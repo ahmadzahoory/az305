fd6f6147ac7cbbbedfd282d548afc5bd3763fac3
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2021-12-18 19:27:31 +0530

    Create lab-305-02-code-srv1.ps1

---
+++ lab-305-02-code-srv1.ps1
@@ -0,0 +1,24 @@
+Set execution policy
+Set-ExecutionPolicy Unrestricted -Force
+New-Item -ItemType directory -Path 'C:\temp'
+
+# Install IIS and Web Management Tools.
+Import-Module ServerManager
+install-windowsfeature web-server, web-webserver -IncludeAllSubFeature
+install-windowsfeature web-mgmt-tools
+
+# Download the files for our web application.
+Set-Location -Path C:\inetpub\wwwroot
+
+$shell_app = new-object -com shell.application
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/ahmadzahoory/az304/master/lab-304-03-code-srv1.zip", (Get-Location).Path + "\lab-304-03-code-srv1.zip")
+
+$zipfile = $shell_app.Namespace((Get-Location).Path + "\lab-304-03-code-srv1.zip")
+$destination = $shell_app.Namespace((Get-Location).Path)
+$destination.copyHere($zipfile.items())
+
+# Create the web app in IIS
+New-WebApplication -Name netapp -PhysicalPath c:\inetpub\wwwroot\vnet-app-website -Site "Default Web Site" -force
+
+#Open ICMPv4 Firewall Rule
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" dir=in action=allow enable=yes protocol=icmpv4:8,any
