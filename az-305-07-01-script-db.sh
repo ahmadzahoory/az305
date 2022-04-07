a5425a2a6ae0cedb303523517c3f1361acdaf241
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2022-04-07 18:31:17 +0530

    Update az-305-07-01-script-db.sh

--- az-305-07-01-script-db.sh
+++ az-305-07-01-script-db.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 apt update
 apt install -y mysql-server
-sudo ufw --force enable
-sudo ufw allow 22
-sudo ufw allow 3306
-sudo ufw reload
-sudo systemctl restart mysql
 sudo mysql -e "
 select user,authentication_string,plugin,host from mysql.user;
 alter user 'root'@'localhost' identified with mysql_native_password by 'password';
@@ -19,4 +14,8 @@ use prod_schema;
 create table products (id int NOT NULL AUTO_INCREMENT, name varchar(255), quantity varchar(255), price varchar(255), PRIMARY KEY (id));
 exit"
 sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo ufw --force enable
+sudo ufw allow 22
+sudo ufw allow 3306
+sudo ufw reload
 sudo systemctl restart mysql
